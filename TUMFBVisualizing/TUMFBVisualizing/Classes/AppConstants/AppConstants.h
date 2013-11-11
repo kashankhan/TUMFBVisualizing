18d6868a51f0e7dd557beb3fe465fc717e6f3899
Kashan Khan <kashankhaan@gmail.com>
2013-11-11 21:44:11 +0100

    project setup

---
+++ TUMFBVisualizing/TUMFBVisualizing/Classes/AppConstants/AppConstants.h
@@ -0,0 +1,14 @@
+//
+//  AppConstants.h
+//  TUMFBVisualizing
+//
+//  Created by Kashan Khan on 11/11/2013.
+//  Copyright (c) 2013 Kashan Khan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+/***************** NOTIFICATIONS *****************/
+
+extern NSString* const UIFacebookLoginSuccessfullNotification;
---
+++ TUMFBVisualizing/TUMFBVisualizing/Classes/AppConstants/AppConstants.m
@@ -0,0 +1,14 @@
+//
+//  AppConstants.m
+//  TUMFBVisualizing
+//
+//  Created by Kashan Khan on 11/11/2013.
+//  Copyright (c) 2013 Kashan Khan. All rights reserved.
+//
+
+#import "AppConstants.h"
+
+
+/***************** NOTIFICATIONS *****************/
+
+NSString* const UIFacebookLoginSuccessfullNotification = @"UIFacebookLoginSuccessfullNotification";
